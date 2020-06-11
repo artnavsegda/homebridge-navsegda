60941c73496a3633bba9109bae9f0aa18161008f
Art Navsegda <artnavsegda@gmail.com>
2020-06-11 14:41:29 +0300

    Update platform.ts

--- src/platform.ts
+++ src/platform.ts
@@ -1,7 +1,9 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
-
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 import { ExamplePlatformAccessory } from './platformAccessory';
+
+const net = require('net');
+const events = require('events');
 
 /**
  * HomebridgePlatform
@@ -59,7 +61,7 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
       // generate a unique id for the accessory this should be generated from
       // something globally unique, but constant, for example, the device serial
       // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.UniqueId);
+      const uuid = this.api.hap.uuid.generate(device.uniqueId);
 
       // see if an accessory with the same uuid has already been registered and restored from
       // the cached devices we stored in the `configureAccessory` method above
